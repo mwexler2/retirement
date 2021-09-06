0af5ca40a8b4eeb47f2321648e258beefe3cc26e
Mike Wexler <mike.wexler@gmail.com>
2021-09-06 15:27:58 -0700

    Clean up income tax test cases a bit.

--- visualizer/src/test/java/name/wexler/retirement/visualizer/CashFlowEstimator/IncomeTaxTest.java
+++ visualizer/src/test/java/name/wexler/retirement/visualizer/CashFlowEstimator/IncomeTaxTest.java
@@ -1,52 +1,67 @@
 package name.wexler.retirement.visualizer.CashFlowEstimator;
 
-import name.wexler.retirement.visualizer.AccountReader;
+import name.wexler.retirement.visualizer.*;
+import name.wexler.retirement.visualizer.Asset.Asset;
 import name.wexler.retirement.visualizer.Asset.AssetAccount;
-import name.wexler.retirement.visualizer.Assumptions;
 import name.wexler.retirement.visualizer.CashFlowEstimator.CashFlowEstimator;
 import name.wexler.retirement.visualizer.CashFlowEstimator.IncomeTax;
 import name.wexler.retirement.visualizer.CashFlowFrequency.CashFlowFrequency;
 import name.wexler.retirement.visualizer.CashFlowFrequency.Quarterly;
-import name.wexler.retirement.visualizer.CashFlowSink;
-import name.wexler.retirement.visualizer.Context;
+import name.wexler.retirement.visualizer.CashFlowInstance.Account;
+import name.wexler.retirement.visualizer.CashFlowInstance.CashFlowInstance;
 import name.wexler.retirement.visualizer.Entity.Company;
+import name.wexler.retirement.visualizer.Entity.Entity;
 import name.wexler.retirement.visualizer.Entity.Person;
+import name.wexler.retirement.visualizer.Tables.CashFlowCalendar;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.collections.ListUtil;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by mwexler on 8/13/16.
  */
 public class IncomeTaxTest {
-    private IncomeTax incomeTax;
+    private IncomeTax federalIncomeTax;
+    private IncomeTax stateIncomeTax;
+    private IncomeTax medicareTax;
+    private IncomeTax socialSecurityTax;
+    Assumptions assumptions;
     private Context context;
 
     @Before
     public void setUp() throws Exception {
         context = new Context();
-        context.setAssumptions(new Assumptions());
-        Person payee = new Person(context, "payee1", LocalDate.of(1976, Month.JULY, 4), 65);
-        Person payor = new Person(context, "payor1", LocalDate.of(1945, Month.AUGUST, 14), 65);
-          LocalDate accrueStart = LocalDate.of(2011, Month.MAY, 1);
-        LocalDate accrueEnd = LocalDate.of(2031, Month.APRIL, 1);
-        LocalDate firstPaymentDate = LocalDate.of(accrueStart.getYear(), accrueStart.getMonth(), 14);
-        firstPaymentDate = LocalDate.of(2011, Month.JULY, 12);
+        assumptions = new Assumptions();
+        context.setAssumptions(assumptions);
+        Person payor = new Person(context, "payor1", LocalDate.of(1976, Month.JULY, 4), 65);
+        Company irsPayee = new Company(context, "irs");
+        Company ftbPayee = new Company(context, "ftb");
+        LocalDate now = LocalDate.now();
+        int nextYear = now.getYear() + 1;
+        LocalDate accrueStart = LocalDate.of(nextYear, Month.JANUARY, 1);
+        LocalDate accrueEnd = LocalDate.of(nextYear, Month.DECEMBER, 31);
+        LocalDate firstPaymentDate = LocalDate.of(nextYear, Month.APRIL, 15);
         CashFlowFrequency quarterly =
                 new Quarterly(context, "quarterly-incomeTax1", accrueStart, accrueEnd, firstPaymentDate,
                         CashFlowFrequency.ApportionmentPeriod.ANNUAL);
         Company bank = new Company(context, "bank1");
         CashFlowSink defaultSink = new AssetAccount(context, "checking1", Arrays.asList(payor.getId()),
                 "Checking account 1", bank.getId(), Collections.emptyList(), null, AccountReader.mintTxnSource);
-        incomeTax = new IncomeTax(context, "incomeTax1", payee.getId(), payor.getId(), quarterly.getId(), defaultSink.getId());
+        federalIncomeTax = new IncomeTax(context, "federalIncomeTax", irsPayee.getId(), payor.getId(), quarterly.getId(), defaultSink.getId());
+        stateIncomeTax = new IncomeTax(context, "stateIncomeTax", ftbPayee.getId(), payor.getId(), quarterly.getId(), defaultSink.getId());
+        medicareTax = new IncomeTax(context, "medicareTax", irsPayee.getId(), payor.getId(), quarterly.getId(), defaultSink.getId());
+        socialSecurityTax = new IncomeTax(context, "socialSecurityTax", irsPayee.getId(), payor.getId(), quarterly.getId(), defaultSink.getId());
     }
 
     @After
@@ -56,28 +71,57 @@ public class IncomeTaxTest {
 
     @Test
     public void getId() {
-        String name1 = incomeTax.getId();
-        assertEquals(name1, "incomeTax1");
+        String name1 = federalIncomeTax.getId();
+        assertEquals("federalIncomeTax", name1);
     }
 
 
     @Test
     public void equals() {
-        assertEquals(incomeTax, incomeTax);
+        assertEquals(federalIncomeTax, federalIncomeTax);
+    }
+
+    @Test
+    public void equalsDifferent() {
+        assertNotEquals(federalIncomeTax, stateIncomeTax);
     }
 
     @Test
     public void toJSON() throws Exception {
-        String expenseSource1Str = context.toJSON(incomeTax);
-        assertEquals("{\"type\":\"incomeTax\",\"id\":\"incomeTax1\",\"payee\":\"payee1\",\"payor\":\"payor1\",\"cashFlow\":\"quarterly-incomeTax1\",\"category\":\"" + incomeTax.INCOME_TAX + "\"}", expenseSource1Str);
+        String expenseSource1Str = context.toJSON(federalIncomeTax);
+        assertEquals("{\"type\":\"incomeTax\",\"id\":\"federalIncomeTax\",\"payee\":\"irs\",\"payor\":\"payor1\",\"cashFlow\":\"quarterly-incomeTax1\",\"category\":\"" + federalIncomeTax.INCOME_TAX + "\"}", expenseSource1Str);
     }
 
 
     @Test
     public void deserialize() throws Exception {
-        String expenseSource1aStr = "{\"type\":\"incomeTax\",\"id\":\"incomeTax1a\",\"payee\":\"payee1\",\"payor\":\"payor1\",\"cashFlow\":\"quarterly-incomeTax1\",\"defaultSink\":\"orSwim\",\"category\":\"" + incomeTax.INCOME_TAX + "\"}";
+        String expenseSource1aStr = "{\"type\":\"incomeTax\",\"id\":\"incomeTax1a\",\"payee\":\"payee1\",\"payor\":\"payor1\",\"cashFlow\":\"quarterly-incomeTax1\",\"defaultSink\":\"orSwim\",\"category\":\"" + federalIncomeTax.INCOME_TAX + "\"}";
         CashFlowEstimator expenseSource1a = context.fromJSON(CashFlowEstimator.class, expenseSource1aStr);
         assertEquals("incomeTax1a", expenseSource1a.getId());
     }
 
+    @Test
+    public void getEstimatedFutureCashFlows() throws Entity.DuplicateEntityException {
+        String[] cashFlowEstimators = new String[] {
+                federalIncomeTax.getId(),
+                stateIncomeTax.getId(),
+                medicareTax.getId(),
+                socialSecurityTax.getId()
+        };
+        String[] assets = new String[] {};
+        String[] liabilities = new String[] {};
+        String[] accounts = new String[] {};
+        Assumptions assumptions = new Assumptions();
+        Scenario scenario = new Scenario(context, "incomeTaxScenario","Income Tax Scenario",
+                cashFlowEstimators, assets, liabilities, accounts, assumptions);
+        CashFlowCalendar cashFlowCalendar = new CashFlowCalendar(scenario, assumptions);
+        List<CashFlowInstance> federalIncomeTaxCashFlows = federalIncomeTax.getEstimatedFutureCashFlows(cashFlowCalendar);
+        assertEquals(4, federalIncomeTaxCashFlows.size());
+        List<CashFlowInstance> stateIncomeTaxCashFlows = stateIncomeTax.getEstimatedFutureCashFlows(cashFlowCalendar);
+        assertEquals(4, stateIncomeTaxCashFlows.size());
+        List<CashFlowInstance> medicareTaxCashFlows = medicareTax.getEstimatedFutureCashFlows(cashFlowCalendar);
+        assertEquals(4, medicareTaxCashFlows.size());
+        List<CashFlowInstance> socialSecurityTaxCashFlows = socialSecurityTax.getEstimatedFutureCashFlows(cashFlowCalendar);
+        assertEquals(4, socialSecurityTaxCashFlows.size());
+    }
 }
